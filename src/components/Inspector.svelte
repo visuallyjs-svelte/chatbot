0cc2e6de27fdaa20988a36d0f1aba46631836637
Simon Porritt <simon@jsplumbtoolkit.com>
2026-07-10 13:22:35 +1000

    updated to use new 2-way state approach with inspector

--- src/components/Inspector.svelte
+++ src/components/Inspector.svelte
@@ -8,55 +8,52 @@
   const CHOICE_PORT = "choicePort";
   const EDGE = "edge";
 
-  let currentType = '';
+  // 2-way prop to share with inspector
+  let current:Base|null = $state(null)
+  // we derive an internal type to use based on whether the current object is a node, port or edge
+  let currentType = $derived(current == null ? null : isNode(current) ? current.data.type as string : isPort(current) ? CHOICE_PORT : EDGE)
 
-  function renderEmptyContainer() {
-    currentType = '';
-  }
-
-  function refresh(obj:Base) {
-    currentType = isNode(obj) ? obj.data.type : isPort(obj) ? CHOICE_PORT : EDGE;
-  }
 </script>
 
-<InspectorComponent {refresh} {renderEmptyContainer}>
-  {#if currentType === ''}
-    <div></div>
-  {:else if currentType === START}
-    <div></div>
-  {:else if currentType === END}
-    <div></div>
-  {:else if currentType === ACTION_MESSAGE}
-    <div class="vjs-chatbot-inspector">
-      <span>Message:</span>
-      <input type="text" vjs-att="message" placeholder="message" vjs-focus="true" />
-    </div>
-  {:else if currentType === ACTION_CHOICE}
-    <div class="vjs-chatbot-inspector">
-      <span>Message:</span>
-      <input type="text" vjs-att="message" placeholder="message" vjs-focus="true" />
-    </div>
-  {:else if currentType === ACTION_TEST}
-    <div class="vjs-chatbot-inspector">
-      <span>Message:</span>
-      <input type="text" vjs-att="message" placeholder="message" vjs-focus="true" />
-    </div>
-  {:else if currentType === ACTION_INPUT}
-    <div class="vjs-chatbot-inspector">
-      <span>Message:</span>
-      <input type="text" vjs-att="message" placeholder="message" />
-      <span>Prompt:</span>
-      <input type="text" vjs-att="prompt" placeholder="prompt" />
-    </div>
-  {:else if currentType === CHOICE_PORT}
-    <div class="vjs-chatbot-inspector">
-      <span>Label:</span>
-      <input type="text" vjs-att="label" vjs-focus="true" placeholder="enter label..." />
-    </div>
-  {:else if currentType === EDGE}
-    <div class="vjs-chatbot-inspector">
-      <div>Label</div>
-      <input type="text" vjs-att="label" vjs-focus="true" />
-    </div>
-  {/if}
+<!-- create inspector component and pass the two-way `current` prop -->
+<InspectorComponent bind:current={current}>
+	{#if currentType == null}
+		<div></div>
+	{:else if currentType === START}
+		<div></div>
+	{:else if currentType === END}
+		<div></div>
+	{:else if currentType === ACTION_MESSAGE}
+		<div class="vjs-chatbot-inspector">
+			<span>Message:</span>
+			<input type="text" vjs-att="message" placeholder="message" vjs-focus="true" />
+		</div>
+	{:else if currentType === ACTION_CHOICE}
+		<div class="vjs-chatbot-inspector">
+			<span>Message:</span>
+			<input type="text" vjs-att="message" placeholder="message" vjs-focus="true" />
+		</div>
+	{:else if currentType === ACTION_TEST}
+		<div class="vjs-chatbot-inspector">
+			<span>Message:</span>
+			<input type="text" vjs-att="message" placeholder="message" vjs-focus="true" />
+		</div>
+	{:else if currentType === ACTION_INPUT}
+		<div class="vjs-chatbot-inspector">
+			<span>Message:</span>
+			<input type="text" vjs-att="message" placeholder="message" />
+			<span>Prompt:</span>
+			<input type="text" vjs-att="prompt" placeholder="prompt" />
+		</div>
+	{:else if currentType === CHOICE_PORT}
+		<div class="vjs-chatbot-inspector">
+			<span>Label:</span>
+			<input type="text" vjs-att="label" vjs-focus="true" placeholder="enter label..." />
+		</div>
+	{:else if currentType === EDGE}
+		<div class="vjs-chatbot-inspector">
+			<div>Label</div>
+			<input type="text" vjs-att="label" vjs-focus="true" />
+		</div>
+	{/if}
 </InspectorComponent>
